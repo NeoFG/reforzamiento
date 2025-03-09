74c1c4363573dfd804657b4086e5f61335c06f2a
Valeria Garzón <valgarna@gmail.com>
2025-03-09 16:16:59 -0600

    Avance curso ionic hasta video 78

--- 03-componentes/componentes/src/app/services/data.service.ts
+++ 03-componentes/componentes/src/app/services/data.service.ts
@@ -1,7 +1,7 @@
 import { Componente } from './../interfaces/interfaces';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import {delay} from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
@@ -26,7 +26,10 @@ export class DataService {
 
   getHeroes(){
 
-    return this.http.get<Componente[]>('/assets/data/superheroes.json');
+    return this.http.get<Componente[]>('/assets/data/superheroes.json')
+          .pipe(
+           delay(1500)
+          );
   }
 
 }
