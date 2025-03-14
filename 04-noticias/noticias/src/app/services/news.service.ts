ea74d96e5529a98221248224500086cf87965a5a
Valeria Garzón <valgarna@gmail.com>
2025-03-14 14:18:44 -0600

    Video 114

--- 04-noticias/noticias/src/app/services/news.service.ts
+++ 04-noticias/noticias/src/app/services/news.service.ts
@@ -1,9 +1,10 @@
-import { Article, NewsResponse } from './../interfaces';
+import { Article, NewsResponse, ArticlesByCategoryAndPage } from './../interfaces';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+
 
 const apiKey = environment.apiKey;
 
@@ -12,10 +13,25 @@ const apiKey = environment.apiKey;
 })
 export class NewsService {
 
+  private articlesByCategoryAndPage: ArticlesByCategoryAndPage = {}
+
+
+
   constructor( private http: HttpClient) { }
 
-  getTopHeadlines():Observable<Article[]>{
+  private executeQuery<T>(endpoint: string): Observable<T> {
+    const url = `https://newsapi.org/v2${endpoint}`;
 
+    return this.http.get<T>(url, {
+      params: {
+        apiKey: apiKey
+      }
+    });
+  }
+
+
+  getTopHeadlines():Observable<Article[]>{
+    console.log('Petición HTTP realizada');
     return this.http.get<NewsResponse>('https://newsapi.org/v2/top-headlines?country=us&category=business',{
       params:{
         apiKey: apiKey
@@ -25,7 +41,8 @@ export class NewsService {
     );
   }
 
-  getTopHeadlinesByCategory(category: string):Observable<Article[]>{
+
+  getTopHeadlinesByCategory(category: string, loadMore: boolean = false):Observable<Article[]>{
     return this.http.get<NewsResponse>(`https://newsapi.org/v2/top-headlines?country=us&category=${category}`,{
       params:{
         apiKey: apiKey
@@ -34,4 +51,29 @@ export class NewsService {
       map(({articles}) => articles)
     );
   }
+
+private getArticlesByCategory (category: string) : Observable<Article[]>{
+
+  if ( Object.keys( this.articlesByCategoryAndPage).includes(category)) {
+    //Ya existe
+   // this.articlesByCategoryAndPage[category].page += 0;
+  } else {
+    //No existe
+    this.articlesByCategoryAndPage[category] = {
+      page : 0,
+      articles: []
+    }
+  }
+
+  const page = this.articlesByCategoryAndPage[category].page + 1;
+
+  return this.executeQuery<NewsResponse>(`/top-headlines?category=${ category }&page=${ page }`)
+  .pipe(
+    map(({ articles }) => articles)
+  );
+
+      }
+
 }
+
+
