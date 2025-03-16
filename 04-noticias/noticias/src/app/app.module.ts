a1f39f9958edf1cb718efe1ea74d296baf93a47c
NeoFG <fernando.garza1@alumno.buap.mx>
2025-03-16 17:44:20 -0600

    App de noticias terminada

--- 04-noticias/noticias/src/app/app.module.ts
+++ 04-noticias/noticias/src/app/app.module.ts
@@ -10,15 +10,21 @@ import { AppComponent } from './app.component';
 
 //Plugins
 import { InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+//Importamos nuestro Social Sharing plugin 
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    IonicStorageModule.forRoot(),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    InAppBrowser,
+    InAppBrowser, SocialSharing
   ],
   bootstrap: [AppComponent],
 })
--- 04-noticias/noticias/src/app/components/article/article.component.ts
+++ 04-noticias/noticias/src/app/components/article/article.component.ts
@@ -1,8 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { CommonModule } from '@angular/common';
 import { Article } from 'src/app/interfaces';
-import { IonicModule, Platform, ActionSheetController } from '@ionic/angular';
+import { IonicModule, Platform, ActionSheetController, ActionSheetButton } from '@ionic/angular';
+//Importamos nuestro Social Sharing plugin 
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-article',
@@ -21,62 +24,79 @@ export class ArticleComponent {
     private iab: InAppBrowser,
     private platform: Platform,
     private actionSheetController: ActionSheetController,
+    private socialSharing: SocialSharing,
+    private storageService: StorageService
   ) { }
 
 
-  openArticle(){
+  openArticle() {
 
-  if ( this.platform.is('ios') || this.platform.is('android')) {
+    if (this.platform.is('ios') || this.platform.is('android')) {
 
-  }
-  const browser = this.iab.create( this.article.url);
-  browser.show();
-  return;
-
-   window.open(this.article.url, '_blank');
-  }
-
-  async onOpenMenu(){
-
-   const actionsheet = await this.actionSheetController.create({
-    header: 'Opciones',
-    buttons: [
-     {
-       text: 'Compartir',
-       icon: 'share-outline',
-       handler: ()=> this.onShareArticle()
-
-     },
-     {
-
-      text: 'Favorito',
-      icon: 'heart-outline',
-      handler: () => this.onToggleFavorite()
-     },
-     {
-     text: 'Cancelar',
-     icon: 'close-outline',
-     role:'cancel',
     }
+    const browser = this.iab.create(this.article.url);
+    browser.show();
+    return;
+
+    window.open(this.article.url, '_blank');
+  }
+
+  async onOpenMenu() {
+
+    const articleInFavorites = this.storageService.articleInFavorites(this.article);
+
+    const normalBtns: ActionSheetButton[] = [
+      {
+
+        text: articleInFavorites? 'Remover favorito' : 'Favorito', 
+        icon: articleInFavorites ? 'heart':'heart-outline',
+        handler: () => this.onToggleFavorite()
+      },
+      {
+        text: 'Cancelar',
+        icon: 'close-outline',
+        role: 'cancel',
+      }
     ]
-   });
 
-   await actionsheet.present();
+    const shareBtn: ActionSheetButton = {
+      text: 'Compartir',
+      icon: 'share-outline',
+      handler: () => this.onShareArticle()
+    };
 
+    if (this.platform.is('capacitor')) {
+      normalBtns.unshift(shareBtn);
+    }
 
+    const actionsheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      buttons: normalBtns
+    });
+
+    
+
+    await actionsheet.present();
   }
 
   onShareArticle() {
-   console.log('share article')
+    const { title, source, url } = this.article;
+
+    this.socialSharing.share(
+      title,
+      source.name,
+      undefined,
+      url
+    );
   }
 
   onToggleFavorite() {
-    console.log('toggle favorie');
+    this.storageService.saveRemoveArticle(this.article);
   }
 
 
   //onClick() {
-   // console.log('El botón fue presionado');
-// }
+  // console.log('El botón fue presionado');
+  // }
 
 }
